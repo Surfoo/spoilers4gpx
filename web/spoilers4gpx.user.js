14c013b252b2c07732f82e88f591dbec163ad831
Surfoo <surfooo@gmail.com>
2013-10-16 18:34:06 +0200

    Update for the greasemonkey script

--- web/spoilers4gpx.user.js
+++ web/spoilers4gpx.user.js
@@ -8,77 +8,81 @@
 // @version         1.0
 // ==/UserScript==
 
-
 (function() {
+    "use strict";
 
     function getURLParameter(name) {
         return decodeURI(
-            (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search) || [, null])[1]
+            (new RegExp(name + '=' + '(.+?)(&|$)').exec(location.search) || [, null])[1]
         );
     }
 
     var elm = document.getElementById('ctl00_ContentBody_GeoNav_adminTools');
     if (elm) {
+        var newLink = document.createElement('a'),
 
-        var newLink = document.createElement('a');
-
-        href = document.createAttribute('href');
+            href = document.createAttribute('href');
         href.nodeValue = '#';
         newLink.setAttributeNode(href);
 
-        style = document.createAttribute('style');
-        style.nodeValue = 'background-image: url("http://spoilers4gpx.vaguelibre.net/spoilers4gpx.png");'
+        var style = document.createAttribute('style');
+        style.nodeValue = 'background-image: url("http://spoilers4gpx.vaguelibre.net/spoilers4gpx.png");';
         newLink.setAttributeNode(style);
 
         var newContent = document.createTextNode("Spoilers4Gpx");
         newLink.appendChild(newContent);
 
-        var newAdminTool = document.createElement("li");
-        attr = document.createAttribute("id");
+        var newAdminTool = document.createElement("li"),
+            attr = document.createAttribute("id");
         attr.nodeValue = "Spoilers4Gpx";
         newAdminTool.setAttributeNode(attr);
 
         newAdminTool.appendChild(newLink);
         elm.appendChild(newAdminTool);
 
-        document.getElementById("Spoilers4Gpx").addEventListener('click', function() {
-            var findSpoilers = /<a href="(http:\/\/img\.geocaching\.com[^.]+\.jpg|jpeg|png|gif)"[^>]+>([^<]+)<\/a>/g
-            var _item = '';
+        document.getElementById("Spoilers4Gpx").addEventListener('click', function(e) {
+            var findSpoilers = /<a href="(http:\/\/img\.geocaching\.com[^.]+\.(jpg|jpeg|png|gif))"[^>]+>([^<]+)<\/a>/g,
+                item = '',
+                list = '',
+                edit_path = '',
+                match;
 
             while (match = findSpoilers.exec(document.documentElement.innerHTML)) {
-                _item += '<!-- Spoiler4Gpx [' + match[2] + '](' + match[1] + ') -->' + "\n";
+                item += '<!-- Spoiler4Gpx [' + match[3] + '](' + match[1] + ') -->' + "\n";
             }
-            if (_item !== null) {
-                var list = 'Here the code to copy paste at the end of the long description, don\'t forget to remove pictures in the list aren\'t a spoiler.' + "\n\n";
-                list += '<!-- Spoiler4Gpx is a tool for include spoilers into GPX files. More info here : http://spoilers4gpx.vaguelibre.net -->' + "\n";
-                list += _item + "\n";
-                list += 'Do you want to go to the edit page now?' + "\n";
+            if (item === '') {
+                alert('No spoilers found :-(');
+                e.preventDefault();
+                return false;
+            }
 
-                var edit_path = 'http://www.geocaching.com' + elm.childNodes[3].childNodes[0].getAttribute("href") + '&s=spoilers4gpx#tbLongDesc';
+            list = 'Here the code to copy paste at the end of the long description, don\'t forget to remove pictures in the list aren\'t a spoiler.' + "\n\n";
+            list += '<!-- Spoiler4Gpx is a tool for include spoilers into GPX files. More info here : http://spoilers4gpx.vaguelibre.net -->' + "\n";
+            list += item + "\n";
+            list += 'Do you want to go to the edit page now?' + "\n";
 
-                if (confirm(list)) {
-                    window.location = edit_path;
-                }
+            edit_path = 'http://www.geocaching.com' + elm.childNodes[3].childNodes[0].getAttribute("href") + '&s=spoilers4gpx#tbLongDesc';
+
+            if (confirm(list)) {
+                window.location = edit_path;
             }
         });
     }
 
-    if (window.location.pathname.substr(1) == 'hide/report.aspx') {
-        if (getURLParameter('s') === 'spoilers4gpx') {
-            var chkIsHtml = document.getElementById('chkIsHtml'),
-                chkUnderstand = document.getElementById('ctl00_ContentBody_chkUnderstand'),
-                chkDisclaimer = document.getElementById('ctl00_ContentBody_chkDisclaimer');
-            if (chkIsHtml && !chkIsHtml.checked) {
-                chkIsHtml.checked = true;
-            }
-            if (chkUnderstand && !chkUnderstand.checked) {
-                chkUnderstand.checked = true;
-            }
-            if (chkDisclaimer && !chkDisclaimer.checked) {
-                chkDisclaimer.checked = true;
-            }
+    if (window.location.pathname.substr(1) === 'hide/report.aspx' && getURLParameter('s') === 'spoilers4gpx') {
+        var chkIsHtml = document.getElementById('chkIsHtml'),
+            chkUnderstand = document.getElementById('ctl00_ContentBody_chkUnderstand'),
+            chkDisclaimer = document.getElementById('ctl00_ContentBody_chkDisclaimer');
+
+        if (chkIsHtml && !chkIsHtml.checked) {
+            chkIsHtml.checked = true;
+        }
+        if (chkUnderstand && !chkUnderstand.checked) {
+            chkUnderstand.checked = true;
+        }
+        if (chkDisclaimer && !chkDisclaimer.checked) {
+            chkDisclaimer.checked = true;
         }
     }
 
-
-}())
+}());
